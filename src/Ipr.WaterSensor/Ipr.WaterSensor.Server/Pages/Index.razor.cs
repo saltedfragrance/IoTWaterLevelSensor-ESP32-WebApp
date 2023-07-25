f6b24c017ec41ec3bfd17841b717b22f4a7850f4
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2023-07-25 13:08:40 +0200

    Add code to calculate water tank level

---
+++ src/Ipr.WaterSensor/Ipr.WaterSensor.Server/Pages/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Ipr.WaterSensor.Server.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public const int waterTankHeight = 400;
+        public int CurrentMeasurementCm { get; set; }
+        public int CurrentWaterLevelCm { get; set; }
+
+        public Index()
+        {
+            CurrentWaterLevelCm = CalculateWaterLevel();
+        }
+
+        private int CalculateWaterLevel()
+        {
+            return waterTankHeight - CurrentMeasurementCm;
+        }
+    }
+}
