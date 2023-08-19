f9467a781eca4992dbcb4f9fbae2611a66423e01
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2023-08-19 14:24:31 +0200

    Add emails page

---
+++ src/Ipr.WaterSensor/Ipr.WaterSensor.Server/Pages/Emails.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ipr.WaterSensor.Server.Pages
+{
+    public class EmailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
